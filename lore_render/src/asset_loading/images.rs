8bb93a75220a260d1a5299cd1c2c31866b1093f1
sepia <soerxpso@gmail.com>
2022-04-09 23:58:35 -0500

    made default texture prettier

--- lore_render/src/asset_loading/images.rs
+++ lore_render/src/asset_loading/images.rs
@@ -22,9 +22,9 @@ pub fn default_texture() -> DynamicImage {
     let black = Rgba([0, 0, 0, 255]);
     let magenta = Rgba([255, 0, 255, 255]);
     let mut img_buf = ImageBuffer::from_pixel(8, 8, black);
-    for x in (0..8).step_by(2) {
-        for y in (0..8).step_by(2) {
-            img_buf.put_pixel(x, y, magenta);
+    for x in (0..8) {
+        for y in (0..7).step_by(2) {
+            img_buf.put_pixel(x, y + (if x % 2 == 0 { 0 } else { 1 }), magenta);
         }
     }
     DynamicImage::ImageRgba8(img_buf)
